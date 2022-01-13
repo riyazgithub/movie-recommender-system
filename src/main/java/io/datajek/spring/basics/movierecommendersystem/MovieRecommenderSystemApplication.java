5a2d0f46ac8cc00ada30dbeaa49c26cb0bee8611
Riyaz Ahmed Afthab <h.a.riyaz@gmail.com>
2022-01-12 19:44:13 -0800

    Initial commit

---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplication.java
@@ -0,0 +1,24 @@
+package io.datajek.spring.basics.movierecommendersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MovieRecommenderSystemApplication {
+
+	public static void main(String[] args) {
+
+		//ApplicationContext manages the beans and dependencies
+		ApplicationContext appContext = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+
+		//Retrieve singleton bean from application context thrice
+		ContentBasedFilter cbf1 = appContext.getBean(ContentBasedFilter.class);
+		ContentBasedFilter cbf2 = appContext.getBean(ContentBasedFilter.class);
+		ContentBasedFilter cbf3= appContext.getBean(ContentBasedFilter.class);
+
+		System.out.println(cbf1);
+		System.out.println(cbf2);
+		System.out.println(cbf3);
+	}
+}
